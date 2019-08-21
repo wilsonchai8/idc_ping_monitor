fa0e3a3afdf198e95f648c5c4f80450948229d52
wilsonchai8 <wilson.chai8@gmail.com>
2019-08-21 15:55:04 +0800

    修复省份显示拼音的问题

--- collection_to_prometheus.py
+++ collection_to_prometheus.py
@@ -48,7 +48,7 @@ def pushMetrics(instance , ISP , key , value):
     headers = {'X-Requested-With': 'Python requests', 'Content-type': 'text/xml'}
     pushgateway = '%s/metrics/job/smokeping-collected-%s/instance/%s' % (paras['prometheus_gateway'] , ISP , instance)
     metrics = 'smokeping_%s{instance=\"%s\" , ISP=\"%s\" , IDC=\"%s\" , alias=\"%s\"} %d' % (key , instance , ISP , 'SH' , paras['province_map'].get(instance) , value)
-    request_code = requests.post(pushgateway , data='{0}\n'.format(metrics) , headers=headers)
+    request_code = requests.post(pushgateway , data='{0}\n'.format(metrics).encode('utf-8') , headers=headers)
     @LogHandler(pushgateway)
     def info():
         return metrics + ' - ' + str(request_code.status_code)
